508a0479c02330ade070de44a851dc0c1ddbc3fa
Biskus <biskus@gmail.com>
2018-12-13 02:24:03 +0100

    Added User authentication and creation

--- flaskserver.py
+++ flaskserver.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from flask import Flask, flash, url_for
-from flask import render_template, redirect
+from flask import render_template, redirect, request
 from flask import send_from_directory
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
@@ -11,16 +12,20 @@ app.config['SECRET_KEY'] = '1cc7a427c58c3837322687b7ff4ee836'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email  = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), unique=True, nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post',
         backref="author",
         lazy=True)
+    
+    def validate_password(self, password):
+        return bcrypt.verify(password, self.password)
+    
 
     def __repr__(self):
         return self.username
@@ -60,13 +65,46 @@ def products():
 def about_us():
     return render_template('about_us.html',info=info)
 
+@app.route("/users")
+def users():
+    return render_template('users.html',info=info, users=User.query.all())
 
 @app.route("/register", methods=['GET','POST'])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Konto opprettet for {form.username.data}!')
+    #if form.validate_on_submit():
+    if request.method == 'POST' and form.validate():
+        if len(User.query.filter_by(username=form.username.data).all()) > 0:
+            flash('Username already exists, please pick another one.',
+                  category="danger")
+            return render_template('register.html',
+                title="Register",
+                form = form,
+                info = info)
+        if len(User.query.filter_by(email=form.email.data).all()) > 0:
+            flash('Email address already exists, please pick another one.',
+                  category="danger")
+            return render_template('register.html',
+                title="Register",
+                form = form,
+                info = info)
+        
+        u = User(
+            username = form.username.data,
+            email = form.email.data,
+            password = bcrypt.generate_password_hash(
+                form.password.data).decode('utf-8')
+            )
+        db.session.add(u)
+        db.session.commit()
+        flash(f'Konto opprettet for {form.username.data}!',
+              category="success")
         return redirect(url_for('home'))
+    elif request.method == 'POST' and not form.validate():
+        flash(f'Ugyldig data, sjekk feil under',
+              category="danger")
+        print(form.errors)
+    
     return render_template('register.html',
         title="Register",
         form = form,
@@ -75,6 +113,24 @@ def register():
 @app.route("/login", methods=['GET','POST'])
 def login():
     form = LoginForm()
+    
+    if request.method == 'POST' and form.validate():
+        user = User.query.filter_by(email=form.email.data).first()
+        print (user)
+        print (type(user))
+        hashedpw = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        if not user:
+            flash(f'User with email {form.email.data} does not exist.')
+            render_template('login.html',
+                title="Login",
+                form = form,
+                info = info)
+        else:
+            print(f"Checking login {user.password} vs {form.password.data} aka {hashedpw}")
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                flash(f'Vellykket tilbake {user.username}!',
+                      category='success')
     return render_template('login.html',
         title="Login",
         form = form,
